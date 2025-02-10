05ec165078b64b34e8f1ae7361fe0ce98de04da6
commonkestrel <jettb6@gmail.com>
2025-02-09 22:46:09 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# PV = nRT
+
+yes this is a super basic gas formula problem solver,
+yes i made this so i could do my homework faster,
+no i don't normally do this kind of work.
---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use std::{io::{self, Write}, str::FromStr};
+
+const R_ATM: f32 = 0.082057338;
+const R_TORR: f32 = 62.363577;
+
+fn main() {
+    let p_in: Option<f32> = input("Pressure").ok();
+    let v_in: Option<f32> = input("Volume (L)").ok();
+    let n_in: Option<f32> = input("Particles (mol)").ok();
+    let r_select: usize = input("R (0 for atm, 1 for torr)").unwrap();
+    let (r, unit) = match r_select {
+        0 => (R_ATM, "atm"),
+        1 => (R_TORR, "torr"),
+        invalid => panic!("invalid R selection: {invalid}"),
+    };
+
+    let t_in: Option<f32> = input("Temperature (K)").ok();
+
+    println!();
+
+    match (p_in, v_in, n_in, t_in) {
+        (None, Some(v), Some(n), Some(t)) => println!("Pressure: {} {unit}", n*r*t/v),
+        (Some(p), None, Some(n), Some(t)) => println!("Volume: {} L", n*r*t/p),
+        (Some(p), Some(v), None, Some(t)) => println!("Particles: {} mol", p*v/r/t),
+        (Some(p), Some(v), Some(n), None) => println!("Temperature: {} K", p*v/n/r),
+        _ => panic!("Must only be missing one input!"),
+    }
+}
+
+fn input<T: FromStr>(prompt: &str) -> Result<T, T::Err> {
+    print!("{prompt}: ");
+    io::stdout().flush().unwrap();
+
+    let mut ret = String::new();
+    io::stdin().read_line(&mut ret).unwrap();
+    return ret.trim().parse();
+}
